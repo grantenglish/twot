9c71c291220622429b3cfceec405e6f065e4a2fa
Grant English <grantenglish@yahoo.com>
2017-11-10 21:51:26 -0500

    removed debug statements

--- twot.py
+++ twot.py
@@ -4,6 +4,7 @@ import requests
 import web
 import json
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
@@ -27,44 +28,35 @@ def getBeers():
     index = 0
 #        beer = [0 for i in range(100)]
     for link in soup.find_all("a", "beername"):
-        print(link.text)
         beer[index] = link.text.strip()
         index = index + 1
 
     index= 0
     for abvelement in soup.find_all("span", "abv"):
-        print(abvelement.text)
         abv[index] = abvelement.text
         index = index + 1
 
     index = 0
     for styleelement in soup.find_all("span", "style"):
-        print(styleelement.text)
         style[index] = styleelement.text
         index = index + 1
 
     index = 0
     for sizeprice in soup.find_all("span", "sizeprice"):
-        print("Start 0")
-        print(sizeprice.text)
         things = sizeprice.text.split()
-        print("Start 1")
-        print(things[0])
         size[index] = things[0]
-        print(things[2])
         price[index] = things[2]
-        print("End")
         index = index + 1
 
     beers = []
     beercount = index
     for i in range(0 , beercount):
-        print(beer[i],style[i],abv[i],size[i],price[i])
         l = [ beer[i],style[i],abv[i],size[i],price[i] ]
         beers.append(l)
 
    # web.header('Content-Type', 'application/json')
-    return json.dumps(beers,sort_keys=True,indent=4, separators=(',', ': '))
+    text = json.dumps(beers,sort_keys=True,indent=4, separators=(',', ': '))
+    return Response(text,  mimetype='application/json')
 
 @app.route('/table')
 def beersTable():
@@ -89,7 +81,6 @@ $(\"#myDummyTable\").DataTable();\
 </thead>\
 <tbody>"
 
-    print(beercount)
     for i in range(0, beercount):
         table = table + "<tr>"
         table = table + "<td>" + beer[i] +"</td>"
