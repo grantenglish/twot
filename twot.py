895b4191c54a90fe434ef695321b2e7a9e9c7dc3
Grant English <grantenglish@yahoo.com>
2017-11-16 10:02:48 -0500

    adding bootstrap

--- twot.py
+++ twot.py
@@ -62,7 +62,10 @@ def getBeers():
 def beersTable():
     global beercount
     table = "" \
-           "     <script type=\"text/javascript\" src=\"http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>\
+            "<script type=\"text/javascript\" src=\"https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js\"></script>\
+            "<script type=\"text/javascript\" src=\"https://cdn.datatables.net/responsive/2.2.0/js/dataTables.responsive.min.js\"></script>\
+            "<script type=\"text/javascript\" src=\"https://cdn.datatables.net/responsive/2.2.0/js/responsive.bootstrap.min.js\"></script>\
+            "<script type=\"text/javascript\" src=\"http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>\
 <script type=\"text/javascript\" src=\"https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js\"></script>\
 <script>\
 $(function(){\
