65768ad5163611fb0626c7d3c6c6d4418637559f
Grant English <grantenglish@yahoo.com>
2017-11-25 08:40:14 -0500

    logo added

--- twot.py
+++ twot.py
@@ -1,15 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-allbeers = []
+allBeers = []
+
+
+@app.route('/twot.png', methods=['GET'])
+def servefile():
+    print( "servefile")
+    return send_from_directory( '', 'twot.png')
+
 
 @app.route('/get')
 def getBeers():
-    global allbeers
+    global allBeers
 
     req  = requests.get("http://thewhiteoaktavern.com/whats-on-tap")
     soup = BeautifulSoup(req.text, "html.parser")
@@ -33,29 +40,29 @@ def getBeers():
         size.append(things[0])
         price.append(things[2])
 
-    allbeers = []
+    allBeers = []
     beercount = len(beer)
     for i in range(0 , beercount-1):
         j = {'type': beer[i], 'style':style[i],'abv':abv[i],'size':size[i],'price':price[i]}
-        allbeers.append(j)
+        allBeers.append(j)
 
-    text = json.dumps(allbeers,sort_keys=True,indent=4, separators=(',', ': '))
+    text = json.dumps(allBeers, sort_keys=True, indent=4, separators=(',', ': '))
     return Response(text,  mimetype='application/json')
 
 @app.route('/')
 def fromFile():
-    global allbeers
+    global allBeers
     with open('beers.json') as json_data:
-        allbeers = json.load(json_data)
+        allBeers = json.load(json_data)
 
-    print(allbeers)
-    text = json.dumps(allbeers, sort_keys=True, indent=4, separators=(',', ': '))
+    print(allBeers)
+    text = json.dumps(allBeers, sort_keys=True, indent=4, separators=(',', ': '))
     return Response(text, mimetype='application/json')
 
 
 @app.route('/table')
 def beersTable():
-    return render_template('simple.html', title="Beers", description='Beers at the Tavern', beers=allbeers)
+    return render_template('simple.html', title="Beers", description='Beers at the Tavern', beers=allBeers)
 
 
 if __name__ == "__main__":
